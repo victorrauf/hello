9822b904396f645a08cdf5013b0d7fcde62b1500
Victor Rauf <victorrauf193@gmail.com>
2024-06-21 06:53:24 -0700

    initial setup

---
+++ app/page.js
@@ -0,0 +1,14 @@
+
+
+export default function Index() {
+  const myName = "Victor";
+
+  return (
+    <div>
+      <h1>My name is {myName}</h1>
+      <p>I am learning ReactJS</p>
+      <p>I am learning ReactJS</p>
+      <p>I am learning ReactJS</p>
+    </div>
+  )
+}
